1e5e77477df623e137e37bc10902b211d94a6cf7
Daniel Ramirez <javamonn@gmail.com>
2020-07-18 13:47:35 -0400

    fix: prune node_modules post-build

--- packages/backend/scripts/postbuild-function.sh
+++ packages/backend/scripts/postbuild-function.sh
@@ -5,19 +5,34 @@ set -ex
 OWN_PATH=`realpath $0`
 FUNCTION_DIR=`realpath "$(dirname $OWN_PATH)/../amplify/backend/function/"`
 
-GRAPHQL_RESOLVER_SRC_DIR="$FUNCTION_DIR/GraphQLResolver/src"
-CLOUDFRONT_ORIGIN_REQUEST_SRC_DIR="$FUNCTION_DIR/CloudfrontOriginRequest/src"
-POST_AUTHENTICATION_SRC_DIR="$FUNCTION_DIR/PostAuthentication/src"
-DYNAMODB_STREAM_SRC_DIR="$FUNCTION_DIR/DynamoDBStream/src"
-SYNC_ANNOTATION_COLLECTION_SRC_DIR="$FUNCTION_DIR/SyncAnnotationCollection/src"
+GRAPHQL_RESOLVER_SRC_DIR="$FUNCTION_DIR/GraphQLResolver"
+CLOUDFRONT_ORIGIN_REQUEST_SRC_DIR="$FUNCTION_DIR/CloudfrontOriginRequest"
+POST_AUTHENTICATION_SRC_DIR="$FUNCTION_DIR/PostAuthentication"
+DYNAMODB_STREAM_SRC_DIR="$FUNCTION_DIR/DynamoDBStream"
+SYNC_ANNOTATION_COLLECTION_SRC_DIR="$FUNCTION_DIR/SyncAnnotationCollection"
 
-# Install any required runtime deps.
-(cd $GRAPHQL_RESOLVER_SRC_DIR && npm install --production)
+# Prune build-time dependencies and install any runtime dependencies
+(cd $GRAPHQL_RESOLVER_SRC_DIR && \
+  rm -rf node_modules && \
+  cd ./src && \
+  npm install --production)
 
-(cd $CLOUDFRONT_ORIGIN_REQUEST_SRC_DIR && npm install --production)
+(cd $CLOUDFRONT_ORIGIN_REQUEST_SRC_DIR && \
+  rm -rf node_modules && \
+  cd ./src && \
+  npm install --production)
 
-(cd $POST_AUTHENTICATION_SRC_DIR && npm install --production)
+(cd $POST_AUTHENTICATION_SRC_DIR && \
+  rm -rf node_modules && \
+  cd ./src && \
+  npm install --production)
 
-(cd $DYNAMODB_STREAM_SRC_DIR && npm install --production)
+(cd $DYNAMODB_STREAM_SRC_DIR && \
+  rm -rf node_modules && \
+  cd ./src && \
+  npm install --production)
 
-(cd $SYNC_ANNOTATION_COLLECTION_SRC_DIR && npm install --production)
+(cd $SYNC_ANNOTATION_COLLECTION_SRC_DIR && \
+  rm -rf node_modules && \
+  cd ./src && \
+  npm install --production)
