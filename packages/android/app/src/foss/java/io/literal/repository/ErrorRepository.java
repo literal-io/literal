c8e6f988daf43a691d8ef3981e5d4904df5f4bb0
Daniel Ramirez <javamonn@gmail.com>
2021-08-01 19:54:42 -0400

    chore(android): add foss and full flavors

---
+++ packages/android/app/src/foss/java/io/literal/repository/ErrorRepository.java
@@ -0,0 +1,42 @@
+package io.literal.repository;
+
+import android.util.Log;
+
+import java.util.Map;
+
+import io.literal.lib.Thunk;
+import io.literal.model.ErrorRepositoryLevel;
+
+public class ErrorRepository {
+
+    public static String CATEGORY_AUTHENTICATION = "Authentication";
+
+    public static Thunk initialize() {
+        Log.d("initialize", "");
+        return () -> { /** noop **/ };
+    }
+
+    public static void captureException(Exception exception) {
+        Log.d("captureException", "", exception);
+    }
+
+    public static void captureException(Exception exception, Map<String, Object> context) {
+        Log.d("captureException", context.toString(), exception);
+    }
+
+    public static void captureException(Throwable exception) {
+        Log.d("captureException", "", exception);
+    }
+
+    public static void captureException(Exception exception, String message) {
+        Log.d("captureException", message, exception);
+    }
+
+    public static void captureWarning(Exception exception) {
+        Log.d("captureWarning", "", exception);
+    }
+
+    public static void captureBreadcrumb(String category, String message, ErrorRepositoryLevel level) {
+        Log.d("captureBreadcrumb", level.name() + ", " + category + ", " + message);
+    }
+}
---
+++ packages/android/app/src/main/java/io/literal/model/ErrorRepositoryLevel.java
@@ -0,0 +1,6 @@
+package io.literal.model;
+
+public enum ErrorRepositoryLevel {
+    INFO,
+    ERROR;
+}
