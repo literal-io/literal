dfdfb76c1455ea800d15b8bb91e7a86d4d05cbb7
Daniel Ramirez <javamonn@gmail.com>
2021-08-24 11:55:58 -0400

    chore: add analytics around annotation renderer initialization events

--- packages/android/app/src/full/java/io/literal/repository/AnalyticsRepository.java
+++ packages/android/app/src/full/java/io/literal/repository/AnalyticsRepository.java
@@ -1,12 +1,18 @@
 package io.literal.repository;
 
 import android.app.Application;
+import android.util.Log;
+
 import com.amplitude.api.Amplitude;
+
 import org.json.JSONObject;
+
+import io.literal.BuildConfig;
 
 public class AnalyticsRepository {
 
     private static final String AMPLITUDE_API_KEY = "8f1701d791829e62f64f1c680c3f78d1";
+
     public static final String TYPE_GRAPH_QL_OPERATION = "GRAPH_QL_OPERATION";
     public static final String TYPE_ACTIVITY_START = "ACTIVITY_START";
     public static final String TYPE_DISPATCHED_WEB_EVENT = "DISPATCHED_WEB_EVENT";
@@ -18,17 +24,29 @@ public class AnalyticsRepository {
     public static final String TYPE_ERROR_REPOSITORY_BREADCRUMB = "ERROR_REPOSITORY_BREADCRUMB";
     public static final String TYPE_ERROR_REPOSITORY_EXCEPTION = "ERROR_REPOSITORY_EXCEPTION";
     public static final String TYPE_ERROR_REPOSITORY_WARNING = "ERROR_REPOSITORY_WARNING";
+    public static final String TYPE_ANNOTATION_RENDERER_FAILED_TO_INITIALIZE = "ANNOTATION_RENDERER_FAILED_TO_INITIALIZE";
+    public static final String TYPE_ANNOTATION_RENDERER_INITIALIZED = "ANNOTATION_RENDERER_INITIALIZED";
 
     public static void initialize(Application application) {
         Amplitude.getInstance().initialize(application.getApplicationContext(), AMPLITUDE_API_KEY)
                 .enableForegroundTracking(application);
+
+        if (BuildConfig.DEBUG) {
+            Log.d("AnalyticsRepository", "initialize");
+        }
     }
 
     public static void logEvent(String type, JSONObject data) {
         Amplitude.getInstance().logEvent(type, data);
+        if (BuildConfig.DEBUG) {
+            Log.d("AnalyticsRepository", "logEvent: " + type + ", " + data.toString());
+        }
     }
 
     public static void setUserId(String userId) {
         Amplitude.getInstance().setUserId(userId);
+        if (BuildConfig.DEBUG) {
+            Log.d("AnalyticsRepository", "setUserId: " + userId);
+        }
     }
 }
